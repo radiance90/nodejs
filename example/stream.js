62c146600c0751ca7a2dda2698afe46d011f1474
radiance90 <henry.j.yu@hotmail.com>
2017-03-17 23:45:46 +1100

    update stream

--- example/stream.js
+++ example/stream.js
@@ -38,3 +38,32 @@ writerStream.on('error', function(err){
 });
 console.log("Program Ended");
 
+
+////piping stream////
+var fs = require("fs");
+// Create a readable stream
+var readerStream = fs.createReadStream('input.txt');
+// Create a writable stream
+var writerStream = fs.createWriteStream('output.txt');
+// Pipe the read and write operations
+// read input.txt and write data to output.txt
+readerStream.pipe(writerStream);
+console.log("Program Ended");
+
+
+////chaining stream////
+var fs = require("fs");
+var zlib = require('zlib');
+// Compress the file input.txt to input.txt.gz
+fs.createReadStream('input.txt')
+   .pipe(zlib.createGzip())
+   .pipe(fs.createWriteStream('input.txt.gz'));
+console.log("File Compressed.");
+
+var fs = require("fs");
+var zlib = require('zlib');
+// Decompress the file input.txt.gz to input.txt
+fs.createReadStream('input.txt.gz')
+   .pipe(zlib.createGunzip())
+   .pipe(fs.createWriteStream('input.txt'));
+console.log("File Decompressed.");
